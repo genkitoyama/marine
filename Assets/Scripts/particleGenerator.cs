919b52f13520a6d19bafbd438fc5a921944c12ed
登山元気 <toyamagenki@tozangenkinoMacBook-Pro.local>
2018-09-27 04:35:44 +0900

    Add scene

---
+++ Assets/Scripts/particleGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleGenerator : MonoBehaviour {
+
+	public int particleNum = 500;
+	public float particleSize = 0.5f;
+
+	public Vector3 size = new Vector3(200f, 50f, 200f);
+
+	// Use this for initialization
+	void Start () {
+
+		var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		Vector3 scale = new Vector3(particleSize, particleSize, particleSize);
+		go.transform.localScale = scale;
+
+		// go.transform.SetParent(transform);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
